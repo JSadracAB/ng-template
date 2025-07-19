e9d3b8432caed6c040f8ca7386d771e726047b53
JSadracAB <jsadracab@gmail.com>
2025-07-19 18:53:55 +0100

    feat: add incremental hydration support and HttpClient provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+  withIncrementalHydration,
+} from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideHttpClient(
+      withInterceptors([
+        // Add any HTTP interceptors here if needed
+      ])
+    ),
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
+    provideClientHydration(withEventReplay(), withIncrementalHydration()),
+  ],
 };
