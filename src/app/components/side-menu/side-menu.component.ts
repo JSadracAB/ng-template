e85d9323524daf39ff0b36dc6d4de3d9f5b61a89
JSadracAB <jsadracab@gmail.com>
2025-08-03 17:03:01 +0100

    feat: add footer, header, and side-menu components with basic structure and tests

---
+++ src/app/components/side-menu/side-menu.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SideMenuComponent {
+
+}
