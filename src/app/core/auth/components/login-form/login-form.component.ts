1af28932d52cf82329cf5dc56c18a14b42b7d9de
JSadracAB <jsadracab@gmail.com>
2025-08-17 16:18:11 +0100

    feat: enhance login form with translation support and validation messages

--- src/app/core/auth/components/login-form/login-form.component.ts
+++ src/app/core/auth/components/login-form/login-form.component.ts
@@ -15,6 +15,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { AuthService } from '../../auth.service';
 import { LoginForm } from './login.form';
 
@@ -31,6 +32,7 @@ import { LoginForm } from './login.form';
     MatCheckboxModule,
     MatAutocompleteModule,
     RouterLink,
+    TranslateModule,
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
